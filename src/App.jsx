7b2655c54c851b366803060958fbaad4aa8bcbe0
Adi Baram <adididi@gmail.com>
2021-09-18 21:32:32 +0300

    web app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App App-header">
+      <header className="App-header">
+        <p>
+          5 Workers are ready to go to work!
+        </p>
+        <button><a href="http://localhost:3000/csv">Get Vehicles Paths</a></button>
+        {/* <button onClick={() => history.push('/csv')}>Get Vehicles Paths</button> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
